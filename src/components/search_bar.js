005bfa7589645e40a1e680b17a2927b4630c80de
Simon <sisihamilton@hotmail.com>
2018-08-03 09:16:38 -0400

    added 4 components and rendering the search bar

---
+++ src/components/search_bar.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+
+// functional/dumb component
+
+// const SearchBar = () => {
+//     return <input />
+// }
+
+// export default SearchBar
+
+
+// class based component - has internal record keeping
+
+class SearchBar extends Component {
+
+    render() { // a method on a class 
+        return <input />
+    }
+
+}
+
+export default SearchBar
+
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+// React is a JS library that is used to produce HTML
+// a compnent is written in JS but produces HTML
+// JSX is a subset of JS
 
-import App from './components/app';
-import reducers from './reducers';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+import SearchBar from './components/search_bar'
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const API_KEY = 'AIzaSyARBgwrsDblJJRIVVBo-e_pRZIDWqjgBwE'
+
+
+// create new component/view - ie. snippet of code that produces html
+const App = () => {  // app is a type of component, we can have many 'instances' of app
+    return (  // first line here must be ( or <div>
+        <div>
+            <h2>React</h2>
+            <SearchBar />
+        </div>
+    )
+}
+
+// render this generated html and put it on the page (in the DOM)
+let root = document.querySelector('.container')
+ReactDOM.render(<App />, root)
